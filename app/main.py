12e2a4139f514f22af792db74ee971d3e807ed81
Hakgyu Kim <zage4448@gmail.com>
2023-06-15 11:42:54 +0900

    refactor: [김학규] 디렉토리 나누기

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from loan_router import loan_router
-from email_authentication_router import email_authentication_router
+from loan.loan_router import loan_router
+from email_authentication.email_authentication_router import email_authentication_router
 
 app = FastAPI()
 
